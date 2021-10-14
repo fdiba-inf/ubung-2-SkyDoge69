8e38d9af6b3bf82c93eed4d1625f443e639579c7
SkyDoge69 <luc2002penta@gmail.com>
2021-10-14 08:28:40 +0300

    Added circle

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,19 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a radius: ");
+        double radius = input.nextDouble();
+
+        double C = 2 * radius * pi;
+        double A = pi * Math.pow(radius, 2);
+
+        System.out.println("Circumference: " + C);
+        System.out.println("Area: " + A);
+    }
+}
